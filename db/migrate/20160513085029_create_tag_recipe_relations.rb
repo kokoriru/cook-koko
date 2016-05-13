4cf067c91e31f232b0e6bdfc406b3ca6ee787f17
kokoriru <kokorirury@yahoo.co.jp>
2016-05-13 18:03:25 +0900

    不要な改行を削除

--- db/migrate/20160513085029_create_tag_recipe_relations.rb
+++ db/migrate/20160513085029_create_tag_recipe_relations.rb
@@ -3,7 +3,6 @@ class CreateTagRecipeRelations < ActiveRecord::Migration
     create_table :tag_recipe_relations do |t|
       t.integer :tag_id
       t.integer :recipe_id
-
       t.timestamps null: false
     end
   end
