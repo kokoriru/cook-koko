326d95e92eec27e9691c95e9979bd61970eaabcd
kokoriru <kokorirury@yahoo.co.jp>
2016-05-16 00:36:34 +0900

    TagCategory の show ページを追加

--- app/controllers/tag_categories_controller.rb
+++ app/controllers/tag_categories_controller.rb
@@ -2,4 +2,8 @@ class TagCategoriesController < ApplicationController
   def index
     @tag_categories = TagCategory.all
   end
+
+  def show
+    @tag_category = TagCategory.find(params[:id])
+  end
 end
