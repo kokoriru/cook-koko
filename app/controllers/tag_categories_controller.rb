259c97ae9fa2ba6be1a853ac893b1976516c6d82
kokoriru <kokorirury@yahoo.co.jp>
2016-05-16 00:53:33 +0900

    TagCategory の new ページを追加

--- app/controllers/tag_categories_controller.rb
+++ app/controllers/tag_categories_controller.rb
@@ -6,4 +6,24 @@ class TagCategoriesController < ApplicationController
   def show
     @tag_category = TagCategory.find(params[:id])
   end
+
+  def new
+    @tag_category = TagCategory.new
+  end
+
+  def create
+    @tag_category = TagCategory.new(tag_category_params)
+
+    if @tag_category.save
+      redirect_to tag_categories_path, notice: 'TagCategory was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tag_category_params
+    params.require(:tag_category).permit(:name)
+  end
 end
