281b92a081aca88b10e58ca23a913e5854b37bf7
kokoriru <kokorirury@yahoo.co.jp>
2016-05-17 23:36:06 +0900

    Recipe#new ページを追加

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,24 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :material, :url, :content, :memo)
+  end
 end
