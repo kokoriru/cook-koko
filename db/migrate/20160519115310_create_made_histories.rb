3f50f5848d94437caf88a85da40213c57b2132b6
kokoriru <kokorirury@yahoo.co.jp>
2016-05-21 01:18:52 +0900

    Recipe#show ページに作ったよボタンを追加

--- db/migrate/20160519115310_create_made_histories.rb
+++ db/migrate/20160519115310_create_made_histories.rb
@@ -1,6 +1,7 @@
 class CreateMadeHistories < ActiveRecord::Migration
   def change
     create_table :made_histories do |t|
+      t.belongs_to :recipe
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20160519115310) do
   enable_extension "plpgsql"
 
   create_table "made_histories", force: :cascade do |t|
+    t.integer  "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
